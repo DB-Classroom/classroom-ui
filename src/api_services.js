617b8cdf0bb698551268bcf360bb2321a1414909
Gowthaman21 <manogowtham211@gmail.com>
2021-08-28 15:20:40 +0530

    Added login functionalities

---
+++ src/api_services.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+async function ApiPostService(link, data) {
+    let url = process.env.REACT_APP_API + link;
+    let res;
+
+    console.log("Link to be sent: ", link);
+
+    try {
+    let Token = process.env.REACT_APP_TOKEN;
+    res = await axios.post(url, data, {
+      headers: {
+        Authorization: Token,
+      },
+    });
+    
+    console.log("Full response: ", res.data);
+
+    if (res.data.success) {
+      console.log("Data Posted!");
+      return res.data;
+    } else {
+      console.log("Req is not completed. ", res.data.err);
+      return res.data;
+    }
+  } catch (error) {
+    console.log("Catch error: ", error);
+    return null;
+  }
+}
+export {ApiPostService};
